d6efb5d96fb16225cfdfd8b614205737cf8f3a2b
nd45 <16ndell@gmail.com>
2022-01-19 10:52:17 -0800

    fix for new dashboard

--- src/Fantohm.js
+++ src/Fantohm.js
@@ -16,29 +16,24 @@ const processDashboardMetrics = (dashboardMetrics) => {
     };
 
     dashboardMetrics.forEach(x => {
-        if (!metrics.price && x.startsWith("Market Price")) {
-            metrics.price = x.replace("Market Price", "");
+        if (!metrics.price && !x.startsWith("MwsFHM Price") && !x.startsWith("FwsFHM Price") && !x.includes("WebsiteGovernanceDocs") && x.startsWith("FHM Price")) {
+            metrics.price = x.substring(x.indexOf("FHM Price") + 9, x.indexOf("Market Cap"));
+            //metrics.marketCap = x.substring(x.indexOf("Market Cap") + 10, x.indexOf("Circulating Supply"));
+            metrics.totalCircSupply = x.substring(x.indexOf("Circulating Supply") + 18, x.indexOf("Global APY"));
+            //metrics.apy = x.substring(x.indexOf("Global APY") + 10, x.indexOf("Treasury Balance"));
+            metrics.marketCap = x.substring(x.indexOf("Treasury Balance") + 16, x.indexOf("Book value"));
         }
-        else if (!metrics.apy && x.startsWith("APY")) {
-            metrics.apy = x.replace("APY", "");
+        if (!metrics.fiveDayRate && x.startsWith("5-Day Rate")) {
+            metrics.fiveDayRate = x.substring(x.indexOf("5-Day Rate") + 10);
         }
-        else if (!metrics.marketCap && x.startsWith("Market Cap")) {
-            metrics.marketCap = x.replace("Market Cap", "");
+        if (!metrics.fiveDayRate && x.startsWith("5-Day Rate")) {
+            metrics.fiveDayRate = x.substring(x.indexOf("5-Day Rate") + 10);
         }
-        else if (!metrics.totalCircSupply && x.startsWith("Circulating Supply (total)")) {
-            metrics.totalCircSupply = x.replace("Circulating Supply (total)", "")
+        if (!metrics.stakedFHM && x.startsWith("Staked FHM") && x.length > 10) {
+            metrics.stakedFHM = x.substring(x.indexOf("Staked FHM") + 10);
         }
-        else if (!metrics.tvl && x.startsWith("TVL")) {
-            metrics.tvl = x.replace("TVL", "");
-        }
-        else if (!metrics.fiveDayRate && x.startsWith("5-Day Rate")) {
-            metrics.fiveDayRate = x.replace("5-Day Rate", "");
-        }
-        else if (!metrics.stakedFHM && x.startsWith("Staked FHM")) {
-            metrics.stakedFHM = x.replace("Staked FHM", "");
-        }
-        else if (!metrics.globalMarketcap && x.startsWith("Global Market Cap")) {
-            metrics.globalMarketcap = x.replace("Global Market Cap", "");
+        if (!metrics.globalMarketcap && x.startsWith("Total Value Deposited") && x.length > 21) {
+            metrics.globalMarketcap = x.substring(x.indexOf("Total Value Deposited") + 21);
         }
     });
 
@@ -90,6 +85,13 @@ const getProtocolMetricsFromWebUI = async () => {
 
     metrics.ftm.rebaseEta = stakingMetrics[0].replace(" to next rebase", "");
 
+    let valueDeposited = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll("h4.MuiTypography-root")).map(x => x.textContent)
+    });
+
+    metrics.ftm.apy = valueDeposited[0];
+    metrics.ftm.tvl = valueDeposited[1];
+
     //Moonriver stats
     await page.goto(CONSTANTS.FHM_STATS_DASHBOARD_URL,
         {
@@ -126,6 +128,19 @@ const getProtocolMetricsFromWebUI = async () => {
     });
 
     metrics.moon.rebaseEta = stakingMetrics[0].replace(" to next rebase", "");
+
+    stakingMetrics = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll("div.rebase-timer")).map(x => x.textContent)
+    });
+
+    metrics.moon.rebaseEta = stakingMetrics[0].replace(" to next rebase", "");
+
+    valueDeposited = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll("h4.MuiTypography-root")).map(x => x.textContent)
+    });
+
+    metrics.moon.apy = valueDeposited[0];
+    metrics.moon.tvl = valueDeposited[1];
 
     await browser.close();
     return metrics;
